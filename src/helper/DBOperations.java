9f19b1078f9e202ec6c86d5ba46c8d005b5cefc5
moizpalitanawala <moizpalitanawala@hotmail.com>
2016-09-24 16:29:50 +0530

    db added

---
+++ src/helper/DBOperations.java
@@ -0,0 +1,100 @@
+package helper;
+
+import org.apache.log4j.Logger;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * Created by moiz.p on 24/09/16.
+ */
+public class DBOperations
+{
+    public static void closeConnection()
+    {
+        DB.closeConnection();
+    }
+
+    public ArrayList<ArrayList<String>> getArrayListFromDB(String sql) throws SQLException {
+        try (Statement st = DB.getConnection().createStatement()) {
+            try (ResultSet rs = st.executeQuery(sql)) {
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int cols = rsmd.getColumnCount();
+                ArrayList<ArrayList<String>> list = new ArrayList<>();
+                while (rs.next()) {
+                    ArrayList<String> row = new ArrayList<>();
+
+                    for (int i = 1; i <= cols; i++) {
+                        row.add(rs.getString(i));
+                    }
+                    list.add(row);
+                }
+
+                return list;
+            }
+        }
+    }
+
+    public ArrayList<String> getListFromDB(String sql) throws SQLException {
+        try (Statement st = DB.getConnection().createStatement()) {
+            try (ResultSet rs = st.executeQuery(sql)) {
+                ArrayList<String> list = new ArrayList<>();
+                while (rs.next()) {
+                    list.add(rs.getString(1));
+                }
+                return list;
+            }
+        }
+    }
+
+    public static class DB
+    {
+        private static final Logger LOGGER = Logger.getLogger(DB.class.getName());
+        private static Connection c = null;
+        public static final String jdbcDriver = "com.mysql.jdbc.Driver";
+        public static final String dbUrl = "jdbc:mysql://127.0.0.1:3306/flockathon";
+        public static String DBUSER = "root", DBPASS = "bruteforce";
+
+        public static Connection getConnection()
+        {
+            if (c == null)
+            {
+                synchronized (DB.class)
+                {
+                    if (c == null)
+                    {
+                        try
+                        {
+                            Class.forName(jdbcDriver);
+                            c = DriverManager.getConnection(dbUrl, DBUSER, DBPASS);
+                        } catch (Exception e)
+                        {
+                            LOGGER.error("Unable to connect to Database!!", e);
+                        }
+                    }
+                }
+            }
+            return c;
+        }
+
+        public static void executeNonQuery(String sql) throws SQLException
+        {
+            try (Statement stmt = c.createStatement())
+            {
+                stmt.executeUpdate(sql);
+            }
+        }
+
+        private static void closeConnection()
+        {
+            try
+            {
+                c.close();
+            } catch (SQLException e)
+            {
+                LOGGER.error("From closeConnection", e);
+            }
+        }
+    }
+
+}
